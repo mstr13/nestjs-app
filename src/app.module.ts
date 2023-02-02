c8abe816ef13073eff445571cf3eca73b9770e6a
Renato <r.mestre@snido.io>
2023-02-02 12:36:59 +0000

    setup typeorm and sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompaniesModule } from './companies/companies.module';
 import { StationsModule } from './stations/stations.module';
 import { StationtypesModule } from './stationtypes/stationtypes.module';
+import { Company } from './companies/entities/company.entity';
+import { Station } from './stations/entities/station.entity';
+import { Stationtype } from './stationtypes/entities/stationtype.entity';
 
+const config: SqliteConnectionOptions = {
+  type: 'sqlite',
+  database: '../db',
+  entities: [Company, Station, Stationtype],
+  synchronize: true,
+}
 @Module({
-  imports: [CompaniesModule, StationsModule, StationtypesModule],
+  imports: [
+    CompaniesModule,
+    StationsModule,
+    StationtypesModule,
+    TypeOrmModule.forRoot(config),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
