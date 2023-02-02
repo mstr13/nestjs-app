9b6d0e88e902a2b5bc0e590de4a38b5ce16d4090
Renato <r.mestre@snido.io>
2023-02-02 10:27:37 +0000

    generate stations boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompaniesModule } from './companies/companies.module';
+import { StationsModule } from './stations/stations.module';
 
 @Module({
-  imports: [CompaniesModule],
+  imports: [CompaniesModule, StationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
